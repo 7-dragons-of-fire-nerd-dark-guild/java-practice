8444226507f444863fe8506f06d80fd0114b2c57
Janos Gyerik <janos@kronos>
2015-12-19 11:39:41 +0100

    removed junk cases

--- src/test/java/com/sevendragons/practice/leetcode/hard/Skyline/SkylineTest.java
+++ src/test/java/com/sevendragons/practice/leetcode/hard/Skyline/SkylineTest.java
@@ -39,21 +39,4 @@ public class SkylineTest {
                 solve(new int[][]{{2, 9, 10}, {3, 7, 15}, {5, 12, 12}})
         );
     }
-
-    @Test
-    public void test_singleton_3() {
-        assertTrue(true);
-    }
-
-    @Test
-    public void test_row_1_2_3() {
-        assertNull(null);
-    }
-
-    @Test
-    public void test_example_1() {
-        // [ [2 9 10], [3 7 15], [5 12 12], [15 20 10], [19 24 8] ]
-        // -> [ [2 10], [3 15], [7 12], [12 0], [15 10], [20 8], [24, 0] ]
-        assertNull(null);
-    }
 }

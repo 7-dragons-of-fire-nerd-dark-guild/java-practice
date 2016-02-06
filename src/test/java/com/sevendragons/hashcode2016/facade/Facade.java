ad5f808033b01d254e6ca97bded121a2927e204a
Janos Gyerik <janos@kronos>
2016-02-06 10:05:27 +0100

    added basic commands

--- src/test/java/com/sevendragons/hashcode2016/facade/Facade.java
+++ src/test/java/com/sevendragons/hashcode2016/facade/Facade.java
@@ -1,4 +1,39 @@
 package com.sevendragons.hashcode2016.facade;
 
 public class Facade {
+    public interface Command {
+
+    }
+
+    public static class PaintSquare implements Command {
+        final int row;
+        final int col;
+        final int size;
+
+        public PaintSquare(int row, int col, int size) {
+            this.row = row;
+            this.col = col;
+            this.size = size;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("PAINT_SQUARE %s %s %s", row, col, size);
+        }
+    }
+
+    public static class EraseCell implements Command {
+        final int row;
+        final int col;
+
+        public EraseCell(int row, int col) {
+            this.row = row;
+            this.col = col;
+        }
+
+        @Override
+        public String toString() {
+            return String.format("ERASE_CELL %s %s", row, col);
+        }
+    }
 }

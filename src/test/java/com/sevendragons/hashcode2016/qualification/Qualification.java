0f1f32113033ab1aa2e1f77141b8f5b3900d93c7
Janos Gyerik <janos@kronos>
2016-02-11 18:28:24 +0100

    added skeleton

--- src/test/java/com/sevendragons/hashcode2016/qualification/Qualification.java
+++ src/test/java/com/sevendragons/hashcode2016/qualification/Qualification.java
@@ -1,5 +1,34 @@
 package com.sevendragons.hashcode2016.qualification;
 
+import java.util.Scanner;
+
 public class Qualification {
+
+    static class Input {
+
+        public static Input fromScanner(Scanner scanner) {
+            return null;
+        }
+    }
+
+    static class Output {
+
+    }
+
+    public static void main(String[] args) {
+        String inputPath = args[0];
+        String outputPath = args[1];
+
+        Input input = parseInput(new Scanner(inputPath));
+        Output output = solve(input);
+    }
+
+    public static Input parseInput(Scanner scanner) {
+        return Input.fromScanner(scanner);
+    }
+
+    public static Output solve(Input input) {
+        return null;
+    }
 
 }

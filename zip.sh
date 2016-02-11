4cdfb1fca308b95f0d6a45244b65f388a9b6fbfb
Janos Gyerik <janos@kronos>
2016-02-11 18:13:57 +0100

    tag with sha and datetime

--- zip.sh
+++ zip.sh
@@ -3,10 +3,12 @@
 branch=qualification
 dir=src/test/java/com/sevendragons/hashcode2016/qualification
 sha=$(git rev-parse HEAD)
+sha=${sha:0:8}
+tag=$sha-$(date +%Y%m%d-%H%M)
 
 cd $(dirname "$0")
 
-zip=submissions/$branch/source-$sha.zip
+zip=submissions/$branch/source-$tag.zip
 mkdir -p $(dirname "$zip")
 
 git archive $branch -o $zip -- $dir

c78926d5c62a85e5c05046bd0adb68a99c5c9555
Janos Gyerik <janos@kronos>
2016-02-11 22:16:28 +0100

    import fix

--- src/test/java/com/sevendragons/hashcode2016/qualification/Qualification.java
+++ src/test/java/com/sevendragons/hashcode2016/qualification/Qualification.java
@@ -2,13 +2,7 @@ package com.sevendragons.hashcode2016.qualification;
 
 import com.sun.org.apache.xpath.internal.operations.Or;
 
-import java.util.ArrayList;
-import java.util.Collections;
-import java.util.HashMap;
-import java.util.List;
-import java.util.Map;
-import java.util.Scanner;
-import java.util.Set;
+import java.util.*;
 
 public class Qualification {
 
